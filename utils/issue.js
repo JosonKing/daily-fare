d07316f467f77ba49fe06d9c3e3f4ae77f67f519
Joson5 <wangsanmu5@163.com>
2020-09-15 13:44:29 +0800

    update add issues

--- utils/issue.js
+++ utils/issue.js
@@ -1,7 +1,8 @@
-const { Octokit } = require("@octokit/core");
-const { createAppAuth } = require("@octokit/auth-app");
+// const { Octokit } = require("@octokit/core");
+// const { createAppAuth } = require("@octokit/auth-app");
+const axios = require('axios');
 
-let secrets = {};
+// let secrets = {};
 
 // try {
 //   secrets = require('../secret.js');
@@ -9,31 +10,34 @@ let secrets = {};
 //   console.log('no secret json, on github action')
 // }
 
-// 6fb26961ac7a2f9dfa89c5cc9690fadc2f054a39
-const octokit = new Octokit({
-  authStrategy: createAppAuth,
-  auth: {
-    id: 80430,
-    // installationId: 11101003,
-    clientId: "Iv1.a5af3e7df8287adb",
-    clientSecret: process.env.clientSecret,
-    privateKey: process.env.privateKey,
-    // clientSecret: process.env.clientSecret ? process.env.clientSecret : secrets.clientSecret,
-    // privateKey: process.env.privateKey ? process.env.privateKey : secrets.privateKey,
-  },
-});
+// const octokit = new Octokit({
+//   authStrategy: createAppAuth,
+//   auth: {
+//     id: 80430,
+//     // installationId: 11101003,
+//     clientId: "Iv1.a5af3e7df8287adb",
+//     clientSecret: process.env.clientSecret,
+//     privateKey: process.env.privateKey,
+//     // clientSecret: process.env.clientSecret ? process.env.clientSecret : secrets.clientSecret,
+//     // privateKey: process.env.privateKey ? process.env.privateKey : secrets.privateKey,
+//   },
+// });
 
-const octokit = new Octokit({ auth: `personal-access-token123` });
+// const octokit = new Octokit({ auth: `personal-access-token123` });
 
 const open = async ({owner, repo, title, body}) => {
   try {    
     console.log('opening issue');
-    const res = await octokit.request('POST /repos/{owner}/{repo}/issues', {
-      owner,
-      repo,
+    const res = await axios.post(`https://api.github.com/repos/${owner}/${repo}/issues?access_token=${process.env.ACCESS_TOKEN}`, {
       title,
-      body,
+      body
     });
+    // const res = await octokit.request('POST /repos/{owner}/{repo}/issues', {
+    //   owner,
+    //   repo,
+    //   title,
+    //   body,
+    // });
     console.log('opened');
     return res;
   } catch (error) {
@@ -44,12 +48,15 @@ const open = async ({owner, repo, title, body}) => {
 
 const lock = async ({owner, repo, issueNumber}) => {
   console.log('locking issue');
-  await octokit.request('PUT /repos/{owner}/{repo}/issues/{issue_number}/lock', {
-    owner: owner,
-    repo: repo,
-    issue_number: issueNumber,
+  await axios.put(`https://api.github.com/repos/${owner}/${repo}/issues/${issueNumber}/lock?access_token=${process.env.ACCESS_TOKEN}`, {
     lock_reason: 'resolved'
-  });
+  })
+  // await octokit.request('PUT /repos/{owner}/{repo}/issues/{issue_number}/lock', {
+  //   owner: owner,
+  //   repo: repo,
+  //   issue_number: issueNumber,
+  //   lock_reason: 'resolved'
+  // });
   console.log('locked');
 }
 
